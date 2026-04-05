ef1899282265e0474b541b8a95de562145ffb144
marilyn <jia.marilyn@gmail.com>
2026-04-05 13:54:30 -0700

    masking function

--- sentence_decoding/grids/test.py
+++ sentence_decoding/grids/test.py
@@ -15,7 +15,7 @@ default_params = {
     "infra.cluster": None,
     "data.n_timelines": 1,
     "data.n_subjects": 10,
-    "save_checkpoints": False,
+    "save_checkpoints": True,
     # "brain_model_config.time_agg_out": "eegnet",
     # "data.feature.model_name": "t5-large",
     # "loss": {"name": "MSELoss", "kwargs": {"reduction": "mean"}},
